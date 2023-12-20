4913a05358b53840cddb77a8be250f804217b96c
LF11235 <leonie.felder@bluewin.ch>
2023-12-21 00:01:05 +0100

    simple stock price graph

--- Utils/Sourcing/DataPresentationStub.py
+++ Utils/Sourcing/DataPresentationStub.py
@@ -2,9 +2,12 @@ import yfinance as yf
 import DataPresentation
 from datetime import date
 from PIL import Image
+import numpy as np 
+import matplotlib.pyplot as plt
 
 def main():
-   stockData = DataPresentation.fetch_stock_data("msft", date(2023,1,1), date(2023,12,31))
+   stock_name = input("Enter the stock name: ")
+   stockData = DataPresentation.fetch_stock_data(stock_name, date(2023,1,1), date(2023,12,31))
    chartPlot = DataPresentation.plot_stock_trend(stockData)
    chartPlot.show()
 
