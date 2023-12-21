171ecc7cff6f93ad1e95838b5ebbe0c9f1ac7996
LF11235 <leonie.felder@bluewin.ch>
2023-12-21 16:16:07 +0100

    add def fetch_ohlc

--- Utils/Sourcing/Yahoo.py
+++ Utils/Sourcing/Yahoo.py
@@ -4,6 +4,56 @@ from datetime import datetime
 import pandas as pd
 import yfinance as yf
 
+def fetch_ohlc(
+    tickers: list | str,
+    start_date: str,
+    end_date: str | None = None,
+    include_dividends: bool = False,
+) -> pd.DataFrame | None:
+    """Function to fetch daily OHLCV data from Yahoo Finance through yfinance
+
+    Parameters
+    ----------
+    tickers : list | str
+        list of Yahoo tickers or Yahoo ticker as str
+    start_date : str
+        First observation date as string in format 'YYYY-MM-DD'
+    end_date : str | None, optional
+        Last observation date as string in format 'YYYY-MM-DD', by default None (converted to today's date)
+    include_dividends: bool
+        Decide whether to include dividends (total returns) or not (price returns)
+
+    Returns
+    -------
+    pd.DataFrame | None
+        DataFrame with Open, High, Low, Close, and Volume
+    """
+    if not end_date:
+        end_date = datetime.now().strftime("%Y-%m-%d")
+
+    if type(tickers) == str:
+        tickers = [tickers]
+
+    temp_download = yf.download(
+        tickers, start=start_date, end=end_date, auto_adjust=True, progress=False
+    )
+
+    if include_dividends:
+        df_dividends = pd.DataFrame()
+        for ticker in tickers:
+            yf_ticker = yf.Ticker(ticker)
+            temp_dividends = pd.DataFrame(yf_ticker.get_dividends())
+            temp_dividends = temp_dividends.rename(columns={"Dividends": ticker})
+            df_dividends = pd.concat([df_dividends, temp_dividends])
+
+        df_dividends.index = pd.to_datetime(df_dividends.index).strftime("%Y-%m-%d")
+        df_dividends = df_dividends[df_dividends.index >= min(temp_download.index)]
+
+        ohlc_data = temp_download.add(df_dividends, fill_value=0.0)
+    else:
+        ohlc_data = temp_download
+
+    return ohlc_data
 
 def fetch_returns(
     tickers: list | str,
@@ -66,6 +116,11 @@ def fetch_returns(
         )
     else:
         returns = temp_close.ffill().pct_change().dropna(how="all")
+        
+    ohlc_data = fetch_ohlc(tickers, start_date, end_date, include_dividends)
+    
+    if not ohlc_data.empty:
+        returns = ohlc_data['Close'].pct_change().dropna(how="all")
 
     if not returns.empty:
         return returns
